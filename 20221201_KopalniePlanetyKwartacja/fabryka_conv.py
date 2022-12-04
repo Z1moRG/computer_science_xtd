19944456dce21f1f9489467b48c26355c01c5450
mgrozy <mgrozy.ssg@gmail.com>
2022-12-04 13:39:39 +0100

    comments

--- 20221201_KopalniePlanetyKwartacja/fabryka_conv.py
+++ 20221201_KopalniePlanetyKwartacja/fabryka_conv.py
@@ -1,36 +1,44 @@
 from datetime import time
 
+# Funkcja zamieniająca wartość z systemu ósemkowego na system kwartański
+# oct_value to wartość w systemie ósemkowym,
+# divider to kwartański podzielnik do wyznaczania liter A, B, C, D,
+# bits to długość ciągu bitowego w liczbie kwartańskiej
 def to_kwart(oct_value, divider, bits):
     dec_value = int(oct_value, 8)
     d = dec_value // divider
-    kwart_letter = chr(d + 65)
+    kwart_letter = chr(d + 65)      # Użycie tablicy ASCII do zamiany liczby na literę 
     reminder = bin(dec_value - d * divider)[2:]
     return kwart_letter + reminder.zfill(bits)
 
+# Funkcja konwertująca czas z systemu ósemkowego na kwartański
 def conv_time(octal_time):
     octal_time = octal_time.split(':')
     return to_kwart(octal_time[0], 24//4, 4) + ':' \
         + to_kwart(octal_time[1], 60//4, 4) + ':' \
         + to_kwart(octal_time[2], 60//4, 4)
 
+# Funkcja konwertująca masę z systemu ósemkowego na kwartański
 def conv_mass(octal_mass):
     return to_kwart(octal_mass, 1000//4, 8)
 
 
+# Otwarcie pliku i wczytanie wszystkich linii z danymi do listy (bez znaków końca linii)
 filename = 'fabryka.txt'
 
 with open(filename) as file_object:
     lines = file_object.read().splitlines()
 
-print(lines)
-
+# Zamiana wartości ilości pomiarów z systemu ósemkowego na dziesiętny
 count = int(lines[0], 8)
 print(count)
 
+# Otwarcie pliku do zapisu danych wyjściowych (skonwertowanych)
 output_filename = 'converted_factory.txt'
 with open(output_filename, 'w') as file_object:
-    file_object.write(f"{bin(count)[2:]}\n")
+    file_object.write(f"{bin(count)[2:]}\n")    # Zapisanie ilości pomiarów w systemie bin
 
+    # Pętla główna
     for n in range(1, count + 1):
         data = lines[n].split()
 
--- 20221201_KopalniePlanetyKwartacja/pomiar_analiza.py
+++ 20221201_KopalniePlanetyKwartacja/pomiar_analiza.py
@@ -8,11 +8,15 @@ def bin_to_dec(numbers):
         decimal_number += int(binary_digit) * 2**power
     return decimal_number
 
+# Funkcja, zamieniająca wartość kwartańską na dziesiętną.
+# Multiplier dla godzin wynosi 24 // 4, dla minut i sekund 60 // 4, dla ton 1000 // 4.
+# Wynika to z zapisu A, B, C, D w systemie kwartańskim.
 def to_dec(kwart_value, multiplier):
-    kwart = ord(kwart_value[0]) - 65
+    kwart = ord(kwart_value[0]) - 65        # Urzycie tablic ASCII to zamiany litery na liczbę
     remainder = bin_to_dec(kwart_value[1:])
     return kwart * multiplier + remainder
 
+# Funkcja zamieniająca czas kwartański na czas ziemski :)
 def conv_time(kwart_time):
     kwart_time = kwart_time.split(':')
     return time( \
@@ -20,38 +24,44 @@ def conv_time(kwart_time):
         minute = to_dec(kwart_time[1], 60//4), \
         second = to_dec(kwart_time[2], 60//4))
 
+# Funkcja zamieniająca masę kwartańską na masę ziemską (tonę) :)
 def conv_mass(kwart_mass):
     return to_dec(kwart_mass, 1000//4)
 
 
+# Otwarcie pliku i wczytanie wszystkich linii z danymi do listy
 filename = 'kopalnia.txt'
 
 with open(filename) as file_object:
     lines = file_object.readlines()
 
-
+# Konwersja z bin na dec ilości rekordów
 count = bin_to_dec(lines[0])
 
-prev_mass = None
-
+# Słownik kategorii pomiarów (zad 1.1)
 measurement = {
     "correct": 0,
     "probable": 0,
     "error": 0
 }
 
+# Inicjalizacja zminnej, która zapamiętuje poprzednią wartość masy (zad 1.2)
+prev_mass = None
+
+# Słownik zapamiętujący długość trwania i ilość kolejnych poprawnych odczytów (zad 1.2)
 continuous_period = {
     'begin_time': time(0, 0, 0),
     'end_time': time(0, 0, 0),
     'counter': 0, 
 }
 
+# Słownik zawierający wynik, czyli najdłuższy okres i największą ilość poprawnych odczytów (zad 1.2)
 longest_period = {
     'period': time(0, 0, 0),
     'counter': 0,
 }
 
-
+# Pętla główna
 for n in range(1, count + 1):
     data = lines[n].split()
 
@@ -63,7 +73,8 @@ for n in range(1, count + 1):
     print(f"{measurement_time}\t\t{current_mass}")
 
     if prev_mass:
-        delta = abs((current_mass - prev_mass) / prev_mass)
+        delta = abs((current_mass - prev_mass) / prev_mass)     # Wyliczenie różnicy pomiaru
+        # Sprawdzenie do jakiego przedziału należy pomiar (zad 1.1)
         if delta <= 0.05:
             measurement['correct'] += 1
             if continuous_period['counter'] == 0:
@@ -78,6 +89,7 @@ for n in range(1, count + 1):
             else:
                 measurement['error'] += 1
                 print("error")
+            # Wyznaczenie najdluższego okresu i ilości pomiarów właściwych (zad 1.2)
             if longest_period['counter'] <= continuous_period['counter']:
                 longest_period['period'] = str( \
                     datetime.combine(date.today(), continuous_period['end_time']) - \
@@ -85,6 +97,7 @@ for n in range(1, count + 1):
                 longest_period['counter'] = continuous_period['counter']
                 continuous_period['counter'] = 0
 
+    # Zapamiętanie wartości masy z poprzedniej iteracji
     prev_mass = current_mass
 
 print("=================================")
